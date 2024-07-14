6ae95789b2b7734e24526ef7b89d36f9a1b0f633
kuruv <kuruv@CHOTU>
2024-07-14 13:15:18 +0530

    <Commiting payments2 to branch1 NetB >

--- src/Payments2.java
+++ src/Payments2.java
@@ -3,4 +3,7 @@ public class Payments2 {
        public String doUpiPayment() {
     	   return "Upi Payment Done";
        }
+       public String doNetBanking() {
+    	   return "Net Banking Ingration is done";
+       }
 }
